e528023b632be0be66661844c49c1f02dc33a20b
lauratan878 <lauratan878@gmail.com>
2018-07-11 18:55:52 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -11,6 +11,13 @@ end
 
 def add_a_key_value_pair
   # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
+   summer_olympics = {
+     :Atlanta => "1996"
+    :Sydney => "2000",
+    :Athens => "2004",
+    :Beijing => "2008",
+    :London => "2012"
+  }
 end
 
 def iterate_through_hash
